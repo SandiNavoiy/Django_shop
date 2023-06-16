e87bb7ba2865a55c8c99ab34c8e9fcdb39bd797f
Sandi_Navoiy <santes.au@gmail.com>
2023-06-16 22:21:10 +0300

    3 задание

--- catalog/views.py
+++ catalog/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        print(request.POST.get('name'))
+        print(request.POST.get('email'))
+        print(request.POST.get('text'))
+
+    return render(request, 'catalog/contacts.html')
