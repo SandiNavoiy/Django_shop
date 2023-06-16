4640b6664d9885f455aa206da01723f37f977e3e
Sandi_Navoiy <santes.au@gmail.com>
2023-06-16 22:38:09 +0300

    Готово

--- catalog/views.py
+++ catalog/views.py
@@ -5,8 +5,8 @@ def index(request):
 
 def contacts(request):
     if request.method == 'POST':
-        print(request.POST.get('name'))
-        print(request.POST.get('email'))
-        print(request.POST.get('text'))
+        print(f"Имя: {request.POST.get('name')}")
+        print(f"Электронная почта: {request.POST.get('email')}")
+        print(f"Текст сообщения: {request.POST.get('text')}")
 
     return render(request, 'catalog/contacts.html')
