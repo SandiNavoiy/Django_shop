733efddf8c367618af5ad4abd1c0362b63c15854
Sandi_Navoiy <santes.au@gmail.com>
2023-06-21 17:09:18 +0300

    6

--- catalog/management/commands/get_to.py
+++ catalog/management/commands/get_to.py
@@ -2,7 +2,7 @@ import json
 
 from django.core.management import BaseCommand
 
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
@@ -14,16 +14,22 @@ class Command(BaseCommand):
         with open('Product_data.json ') as file:
             data = json.load(file)
         products = []
-        for item
+        for item in data:
+            category_name = item['fields']['category_name']
+            category = Category.objects.get(pk = category_name)
+            product = Product(
+                    product_name = item['fields']['product_name'],
+                    description_product = item['fields']['description_product'],
+                    picture_product = item['fields']['picture_product'],
+                    category_name = category,
+                    price_product = item['fields']['price_product'],
+                    date_of_creation_product = item['fields']['date_of_creation_product'],
+                    last_modified_date_product = item['fields']['last_modified_date_product']
+
+                )
+            products.append(product)
+
 
 
         Product.objects.bulk_create(products)
 
-
-            product_name = models.CharField(max_length=100, verbose_name='наименование')
-            description_product = models.TextField(verbose_name='описание')
-            picture_product = models.ImageField(upload_to='blog/', null=True, blank=True, verbose_name='изображение')
-            category_name = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
-            price_product = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='цена за покупку')
-            date_of_creation_product = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
-            last_modified_date_product = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
