8828091ebcb2427d7e90368642339e0ac0518030
Sandi_Navoiy <santes.au@gmail.com>
2023-06-26 20:56:55 +0300

    1

--- catalog/templatetags/mediapath.py
+++ catalog/templatetags/mediapath.py
@@ -1,7 +1,16 @@
 from django import template
 
+from django.conf import settings
+
+
 register = template.Library()
 
 @register.filter
-def mediapath(image_path):
-    return f"/media/{image_path}"
+def mediapath(value):
+    # Получаем путь к медиафайлам из настроек Django
+    media_url = settings.MEDIA_URL
+
+    # Объединяем путь к медиафайлу с базовым URL медиафайлов
+    media_path = f"{media_url}{value}"
+
+    return media_path
