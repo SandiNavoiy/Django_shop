4645023a04a267fdc5d5393b56599b9e5cb0a531
Sandi_Navoiy <santes.au@gmail.com>
2023-06-26 20:05:19 +0300

    Добавление не через админку

--- catalog/templatetags/media_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.conf import settings
-
-register = template.Library()
-
-#@register.image
-#def mediapath(file_path):
-#    media_url = settings.MEDIA_URL
-#    return f"{media_url}{file_path}"
