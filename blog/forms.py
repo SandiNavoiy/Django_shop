c47ae80f72157798df0715f5f70504f472865f1c
Sandi_Navoiy <santes.au@gmail.com>
2023-07-08 23:35:42 +0300

    автослуг

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,5 @@
+from django.utils.text import slugify
+
 from blog.models import BlogPost
 from django import forms
 
@@ -5,4 +7,11 @@ from django import forms
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        exclude = ['created_at', 'views_count', 'slug']
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.slug = slugify(instance.title)
+        if commit:
+            instance.save()
+        return instance
