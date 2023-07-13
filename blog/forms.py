a298e4f956b5922f133326c0c48e988c709c645b
Sandi_Navoiy <santes.au@gmail.com>
2023-07-13 19:21:32 +0300

    2 задание

--- blog/forms.py
+++ blog/forms.py
@@ -9,6 +9,13 @@ class BlogPostForm(forms.ModelForm):
         model = BlogPost
         exclude = ['created_at', 'views_count', 'slug']
 
+    def clean_title(self):
+        """метод валидации по запрерщенным словам"""
+        cleaned_data = self.cleaned_data.get('title')
+        if cleaned_data in ("казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
+            raise forms.ValidationError('Запрещенное слово')
+        return cleaned_data
+
     def save(self, commit=True):
         instance = super().save(commit=False)
         instance.slug = slugify(instance.title)
