25384a04c95f9c2e0078e5ae2358f8abff61ee3d
Sandi_Navoiy <santes.au@gmail.com>
2023-07-13 23:18:40 +0300

    Дикая хрень

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,24 +1,34 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
-from .models import Product, Category
+from django.forms import formset_factory
+
+from .models import Product, Category, Version
+
+
 
 
 class ProductForm(forms.ModelForm):
+
     class Meta:
         """подкласс описания """
         model = Product  # модель
         fields = '__all__'  # поля
 
+
     def clean_product_name(self):
         """метод валидации по запрерщенным словам"""
         cleaned_data = self.cleaned_data.get('product_name')
-        if cleaned_data in("казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
+        if cleaned_data in (
+        "казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
             raise forms.ValidationError('Запрещенное слово')
         return cleaned_data
 
     def clean_description_product(self):
         """метод валидации по заперщенным словам"""
         cleaned_data = self.cleaned_data.get('description_product')
-        if cleaned_data in("казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
+        if cleaned_data in (
+        "казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
             raise forms.ValidationError('Запрещенное слово')
         return cleaned_data
 
@@ -32,13 +42,15 @@ class CategoryForm(forms.ModelForm):
     def clean_category_name(self):
         """метод валидации по запрерщенным словам"""
         cleaned_data = self.cleaned_data.get('category_name')
-        if cleaned_data in("казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
+        if cleaned_data in (
+        "казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
             raise forms.ValidationError('Запрещенное слово')
         return cleaned_data
 
     def clean_description_category(self):
         """метод валидации по заперщенным словам"""
         cleaned_data = self.cleaned_data.get('description_category')
-        if cleaned_data in("казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
+        if cleaned_data in (
+        "казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"):
             raise forms.ValidationError('Запрещенное слово')
         return cleaned_data
