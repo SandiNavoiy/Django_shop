c7013fdfb696bba850b33780e81d5df34f683a02
Sandi_Navoiy <santes.au@gmail.com>
2023-07-15 20:22:07 +0300

    фикс ошибки формы

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 
 from users.apps import UsersConfig
 
+from users.views import RegisterView, UserUpdateView
+
 app_name = UsersConfig.name
 urlpatterns = [
-    path('', LoginView.as_view(template_name='users/login.html'), name='login'),  # вывод главной страницы
+    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('update/', UserUpdateView.as_view(), name='update'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+
+from users.forms import UserRegisterForm, UserUpdate
+from users.models import User
+
 
 # Create your views here.
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = "users/register.html"
+    success_url = reverse_lazy('users:login')
+
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserUpdate
+    template_name = "users/update_user.html"
+    success_url = reverse_lazy('catalog:index')
+
+    def get_object(self, queryset=None):
+        return self.request.user
