b0ed7cf69adc2b77c6de0be218e18c6f6de391a1
Sandi_Navoiy <santes.au@gmail.com>
2023-07-19 23:07:40 +0300

    доработка

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='электронная почта')
     phone = models.CharField(max_length=50, verbose_name='телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
-    country = models.CharField(max_length=20, verbose_name='страна')
+    country = models.CharField(max_length=20, verbose_name='страна', **NULLABLE)
     reset_password_token = models.CharField(max_length=100, blank=True, null=True)
 
     # переопределение поля user  как основного для идентификации на емаил
